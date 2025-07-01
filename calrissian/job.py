c3ca9e4773b350ee36394ab389c3d8f603eac0c0
tjellicoe <thomas.jellicoe@telespazio.com>
2025-07-01 12:47:52 +0100

    Set workspace volume to be read-write

--- calrissian/job.py
+++ calrissian/job.py
@@ -122,7 +122,11 @@ class KubernetesVolumeBuilder(object):
             # Mount any additional volumes not already mounted, only duplicate is "calrissian-wdir"
             if claim_name != "calrissian-wdir":
                 log.info(f"Adding PVC {claim_name} mounted at {mount_path}")
-                self.add_volume_binding(mount_path, mount_path, False)
+                writable = False
+                # Need to allow writing to the workspace directory
+                if claim_name.startswith("pvc-"):
+                    writable = True
+                self.add_volume_binding(mount_path, mount_path, writable=writable)
 
     def add_persistent_volume_entry(self, prefix, sub_path, claim_name, read_only):
         entry = {
